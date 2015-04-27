a4fc512a5f2d086dca6d975adf4f01d6a721a51c
scarlett2003 <jovilee@tpts8.seed.net.tw>
2015-04-28 02:24:48 +0800

    add head function.js

---
+++ js/functions.js
@@ -0,0 +1,20 @@
+$(window).scroll(function(){
+
+	var wScroll = $(this).scrollTop();
+
+	// console.log(wScroll)
+	$('.logo').css({
+		'transform' : 'translate(0px, '+ wScroll /2 +'%)'
+	});
+
+
+	$('.back-bird').css({
+		'transform' : 'translate(0px, '+ wScroll /4 +'%)'
+	});
+
+	$('.fore-bird').css({
+		'transform' : 'translate(0px, -'+ wScroll /40 +'%)'
+	});
+
+
+});
