265a4c24b3f2ddb850a0709c7338443e311a9eb4
vialley <jovilee09@gmail.com>
2015-04-28 20:35:44 +0800

    Parallax On The Web (Part 2~4)

--- js/functions.js
+++ js/functions.js
@@ -17,4 +17,17 @@ $(window).scroll(function(){
 	});
 
 
+	if(wScroll > $('.clothes-pics').offset().top -($(window).height() / 1.2)){
+
+		$('.clothes-pics figure').each(function(i){
+
+			setTimeout(function(){
+				$('.clothes-pics figure').eq(i).addClass('is-showing');				
+			}, 150 * (i+1));
+
+		});
+
+	}
+
+
 });
